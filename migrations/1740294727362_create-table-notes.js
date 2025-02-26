ad3f23c60f3710f8d0bfd37061fc58a2dd8ecf95
Suhandi <suhandiakiats@gmail.com>
2025-02-26 16:31:59 +0700

    install @hapi/jwt, and migrate auth db

--- migrations/1740294727362_create-table-notes.js
+++ migrations/1740294727362_create-table-notes.js
@@ -1,7 +1,13 @@
-/* eslint-disable camelcase */
-
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
 exports.shorthands = undefined;
 
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
 exports.up = (pgm) => {
   pgm.createTable('notes', {
     id: {
@@ -31,6 +37,11 @@ exports.up = (pgm) => {
   });
 };
 
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
 exports.down = (pgm) => {
   pgm.dropTable('notes');
 };
